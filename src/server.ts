8c43a1d745b15343c04ef9bc3327575505d19368
Flavio Francisco <flaviofrancis@msn.com>
2020-06-21 00:46:17 +0200

    aws settings

--- src/server.ts
+++ src/server.ts
@@ -14,16 +14,25 @@ import { V0_FEED_MODELS } from './controllers/v0/model.index';
     await sequelize.sync();
 
     const app = express();
-
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", config.url);
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token");
-        next();
-      });    
-
     const port = process.env.PORT || 8080;
 
     app.use(bodyParser.json());
+
+    // app.use(function(req, res, next) {
+    //     res.header("Access-Control-Allow-Origin", config.url);
+    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Access-Token");
+    //     next();
+    //   });    
+
+    app.use(cors({
+        allowedHeaders: [
+          'Origin', 'X-Requested-With',
+          'Content-Type', 'Accept',
+          'X-Access-Token', 'Authorization',
+        ],
+        methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+        origin: config.url,
+      }));
 
     app.use('/api/v0/', IndexRouter);
 

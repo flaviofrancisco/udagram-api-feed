2f5c667330f971ba18efb39d6aeb44e8e6ef4610
Flavio Francisco <flaviofrancis@msn.com>
2020-06-22 14:04:04 +0200

    aws.ts updated

--- src/aws.ts
+++ src/aws.ts
@@ -1,9 +1,9 @@
 import AWS = require('aws-sdk');
 import { config } from './config/config';
 
-
 // Configure AWS
-AWS.config = AWS.config.loadFromPath('./aws-config.json');
+var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
+AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
     signatureVersion: 'v4',
